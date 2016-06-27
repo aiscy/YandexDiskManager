45874b7772a2872a8037e6b84c5a4c416403825c
aiscy <heher111@gmail.com>
2016-06-27 20:14:36 +0500

    SettingsUI вынесен в отдельный файл

--- yandexapi.py
+++ yandexapi.py
@@ -2,70 +2,19 @@ import logging
 import os
 import sys
 
-from PyQt5.QtCore import QThread, QSettings, pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QApplication, qApp, QDialog, QFileDialog, QMessageBox, QListWidgetItem
+from PyQt5.QtCore import QThread, pyqtSlot
+from PyQt5.QtWidgets import QMainWindow, QApplication, qApp, QDialog, QListWidgetItem
 
 from yandexapi.dialogs.item_list_widget import ItemListWidget
+from yandexapi.dialogs.settings_ui import SettingsUI
 from yandexapi.ui.main_ui import Ui_MainWindow
 
 # from yandexapi.ui.custom_widget import CustomWidget
-from yandexapi.ui.settings_ui import Ui_Dialog
 from yandexapi.workers.new_task import WorkerNewTask
 from yandexapi.workers.yandex_upload import WorkerYandexUpload
 
 
 # from yandexapi.api.yandex_api import YandexAPI
-
-
-class SettingsUI(QDialog, Ui_Dialog):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        self.settings = QSettings('settings.ini', QSettings.IniFormat)
-
-        self.toolButton.clicked.connect(lambda:self.lineEdit_watcher_folder.setText(QFileDialog.getExistingDirectory()))
-
-    def isFirstRun(self):
-        return self.settings.value('first_run', False)
-
-    def getAPIKey(self):
-        return self.settings.value('api_key')
-
-    def getFolder(self):
-        return self.settings.value('folder')
-
-    # def getWhitelistExtensions(self):
-    #     return self.settings.value('files_extension')
-
-    def getMailServer(self):
-        return self.settings.value('mail_server')
-
-    def getMailError(self):
-        return self.settings.value('mail_error')
-
-    def accept(self):
-        if not self.lineEdit_API.text():
-            QMessageBox.critical(self, 'Ошибка', 'Укажите API ключ для доступа к Yandex API')
-        elif not self.lineEdit_watcher_folder.text() or not os.path.isdir(self.lineEdit_watcher_folder.text()):
-            QMessageBox.critical(self, 'Ошибка', 'Укажите папку с файлами для загрузки')
-        elif not self.lineEdit_mail_server.text():
-            QMessageBox.critical(self, 'Ошибка', 'Укажите адрес сервера')
-        else:
-            self.settings.setValue('first_run', False)
-            self.settings.setValue('api_key', self.lineEdit_API.text())
-            self.settings.setValue('folder', self.lineEdit_watcher_folder.text())
-            self.settings.setValue('mail_error', self.lineEdit_mail_error.text())
-            self.settings.setValue('mail_server', self.lineEdit_mail_server.text())
-
-            super().accept()
-
-    def exec(self):
-        self.lineEdit_API.setText(self.getAPIKey())
-        self.lineEdit_watcher_folder.setText(self.getFolder())
-        self.lineEdit_mail_error.setText(self.getMailError())
-        self.lineEdit_mail_server.setText(self.getMailServer())
-        super().exec()
 
 
 class MainUI(QMainWindow, Ui_MainWindow):
---
+++ yandexapi/dialogs/settings_ui.py
@@ -0,0 +1,57 @@
+import os
+
+from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QDialog, QFileDialog, QMessageBox
+
+from yandexapi.ui.settings_ui import Ui_Dialog
+
+
+class SettingsUI(QDialog, Ui_Dialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.settings = QSettings('settings.ini', QSettings.IniFormat)
+
+        self.toolButton.clicked.connect(lambda:self.lineEdit_watcher_folder.setText(QFileDialog.getExistingDirectory()))
+
+    def isFirstRun(self):
+        return self.settings.value('first_run', False)
+
+    def getAPIKey(self):
+        return self.settings.value('api_key')
+
+    def getFolder(self):
+        return self.settings.value('folder')
+
+    # def getWhitelistExtensions(self):
+    #     return self.settings.value('files_extension')
+
+    def getMailServer(self):
+        return self.settings.value('mail_server')
+
+    def getMailError(self):
+        return self.settings.value('mail_error')
+
+    def accept(self):
+        if not self.lineEdit_API.text():
+            QMessageBox.critical(self, 'Ошибка', 'Укажите API ключ для доступа к Yandex API')
+        elif not self.lineEdit_watcher_folder.text() or not os.path.isdir(self.lineEdit_watcher_folder.text()):
+            QMessageBox.critical(self, 'Ошибка', 'Укажите папку с файлами для загрузки')
+        elif not self.lineEdit_mail_server.text():
+            QMessageBox.critical(self, 'Ошибка', 'Укажите адрес сервера')
+        else:
+            self.settings.setValue('first_run', False)
+            self.settings.setValue('api_key', self.lineEdit_API.text())
+            self.settings.setValue('folder', self.lineEdit_watcher_folder.text())
+            self.settings.setValue('mail_error', self.lineEdit_mail_error.text())
+            self.settings.setValue('mail_server', self.lineEdit_mail_server.text())
+
+            super().accept()
+
+    def exec(self):
+        self.lineEdit_API.setText(self.getAPIKey())
+        self.lineEdit_watcher_folder.setText(self.getFolder())
+        self.lineEdit_mail_error.setText(self.getMailError())
+        self.lineEdit_mail_server.setText(self.getMailServer())
+        super().exec()
